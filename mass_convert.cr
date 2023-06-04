6aa404a61a8e067f09c6235fab67bc3e3acd8c15
Sam Johnson <sam@durosoft.com>
2023-06-04 13:17:47 -0400

    working

--- mass_convert.cr
+++ mass_convert.cr
@@ -3,5 +3,8 @@ Dir.open(ARGV[0]).children.each do |f|
   next unless f.ends_with?(".mp3")
   path = Path["#{ARGV[0]}"].join(f)
   puts "processing '#{path}'"
+  #path_tmp = "#{path}_tmp"
+  #puts `mv '#{path}' '#{path_tmp}'`
+  #puts `ffmpeg -loop 1 -y -i ~/Pictures/Jaboody.jpg -i '#{path_tmp}' -shortest '#{path.to_s.gsub(".mp3", ".mkv")}'`
   puts `ffmpeg -i '#{path}' '#{path.to_s.gsub(".mp3", ".mkv")}'`
 end
